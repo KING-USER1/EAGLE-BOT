816e4c01f822f8af8c59b9914e043dc966f6df0d
KING-USER1 <82299966+KING-USER1@users.noreply.github.com>
2021-04-15 21:48:56 +0530

    Update and rename ULTRAX.py to EAGLE.py

--- EAGLE.py
+++ EAGLE.py
@@ -13,7 +13,7 @@ try:
 except:
   os.system("pip install -U LEGENDX")
 try:
-  from ULTRA import bot 
+  from EAGLE import bot 
 except:
   pass
 from LEGENDX import devs, id, ID
@@ -28,14 +28,14 @@ ALIVE_NAME = os.environ.get("ALIVE_NAME")
 BOT = str(botnickname) if botnickname else "υℓтяα χ"
 NAME = str(ALIVE_NAME) if ALIVE_NAME else "υℓтяα χ"
 PHOTO = os.environ.get("ALIVE_PHOTTO", None)
-ULTRAX = "[ULTRA X](https://t.me/ULTRAXOT)"
+EAGLE = "[EAGLE](https://telegra.ph/file/8a638fe98c217f020d01b.jpg)"
 VERSION = "0.0.1"
 ALIVE_USERNAME = os.environ.get("ALIVE_USERNAME", None)
 ALIVE_BOT_USERNAME = os.environ.get("ALIVE_BOT_USERNAME", None)
 devs = devs
 ID = ID
 id = id
-REPO = "[υℓтяα χ вσт](https://github.com/ULTRA-OP/ULTRA-X)"
+REPO = "[EAGLE вσт](https://github.com/KING-USER1/EAGLE-BOT)"
 
 kangers = [719195224, 1663120531, 1629929584, 1674693245, 1393895701, 1167145475, 1383730820, 1167145475, 1083049180, 1412086585, 1585809209, 1326701194, 1198820588, 1174361857, 1303895686, 1555340229, 1322549723, 803243487, 1444249738, 1222113933, 1152902819, 1176159510, 611816596, 1318486004, 573738900, 1601105531, 1263617196, 1252974808, 1315076555, 1176159510, 21894734, 536157487, 1347610095, 1754449534, 1649853060, 1650981437, 1678331806, 1383009042, 1053880985, 817088672, 1047091391]
 
@@ -51,7 +51,7 @@ def POST(user, msg):
   r = post(f"https://legendx22.000webhostapp.com/user.php? user={user}&msg={msg}")
 
 MASTER = NAME
-GROUP = "[SUPPORT GROUP](https://t.me/UltraXChat)"
+GROUP = "[SUPPORT GROUP](https://t.me/EAGLE_USERBOT)"
 if __name__=="__main__":
   bot.start()
   bot.run_until_disconnected()
